afd5a5854a9019fbe43a5d19c58b44383ab8fa10
Connor Quick <connor@zer0w1re.net>
2017-06-30 10:13:10 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+Python Bittrex Pump and Dumper
+=
+
+This program will allow you to type the name of a coin when a Telegram
+Pump and Dump group says the name, and it will automatically place a
+buy order at +10%*ask, and a sell order at +30%*ask. You can change
+these values, but they seem like they will provide safe steady income.
+
+
+Usage
+=
+  Add your API key and secret to pumpdump.py.
+
+  Also, make note of the `allow_order` variable. If set to `True`, it
+  will buy and sell coins. `False`, it will just display values.
+
+  Example:
+
+    $ python pumpdump.py
+			!!! BY DEFAULT THIS WILL BUY AS MANY COINS AS YOU HAVE BTC AVAILABLE !!!
+			!!! PRESS CTRL+C TO CANCEL BEFORE TYPING ANY COIN NAMES IF YOU DON"T WANT TO BUY !!!
+
+			Coin: ETH
+			You have 0.00137032 BTC available.
+			Ask -- 0.11681
+			Ask + 10% (safeish buy point) -- 0.128491
+			Ask + 30% (safeish sell point) -- 0.151853
+
+			[+] Buying 0.0114379077134 ETH coins at 0.128491 BTC each for a total of 0.0014696682 BTC
+      {u'uuid': u'4dceb026-23ef-483a-294e-eb5ce94dbe7c'}
+			[+] Placing sell order at 0.151853 (+30%)...
+      {u'uuid': u'4dceb026-23ef-483a-294e-eb5ce94dbe7c'}
+
+Credits
+=
+
+https://github.com/ndri/python-bittrex
+
+Donate
+=
+
+If you feel like this program helped you out, or made you money, feel free to donate!
+
+BTC: 1Gv2MDC8YgjDYiy6P7sTZizihEKoJyK8ew
+
+Disclaimer
+=
+
+USE AT OWN RISK
+PROFITS NOT GUARENTEED
---
+++ pumpdump.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+"""Python Pump and Dumper
+
+This program allows you to input a cryptocoin name and it will
+calculate a buy point 10% higher than the current 'ask', and a sell
+point at 30% higher than the current 'ask'. While not making a ton of
+profit each time, typing the coin name is much quicker than navigating
+to Bittrex and manually buying and selling. It will be small, but safe
+profit.
+
+!!! BY DEFAULT THIS WILL BUY AS MANY COINS AS YOU HAVE BTC AVAILABLE !!!
+
+Note: This is not guarenteed to make any profit. The markets are
+volitile, and even if a pump is happening, it is possible that it
+could not reach +30%, and thus you could be stuck with worthless coins
+if you don't sell fast enough.
+"""
+
+from bittrex import bittrex
+import sys, signal, json
+
+# Get these from https://bittrex.com/Account/ManageApiKey
+api = bittrex('key', 'secret')
+
+# Set to True to enable limit trading...
+# !!! USE AT OWN RISK !!!
+allow_orders = False
+
+def sigint_handler(signum, frame):
+    print '\n[!] CTRL+C pressed. Exiting...'
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, sigint_handler)
+
+if allow_orders:
+    print '!!! BY DEFAULT THIS WILL BUY AS MANY COINS AS YOU HAVE BTC AVAILABLE !!!'
+    print '!!! PRESS CTRL+C TO CANCEL BEFORE TYPING ANY COIN NAMES IF YOU DON"T WANT TO BUY !!!\n'
+else:
+    print "[!] allow_orders = False in script... change to make orders..."
+
+pumpCoin = raw_input("Coin: ")
+
+coinPrice = api.getticker("BTC-" + pumpCoin)
+askPrice = coinPrice['Ask']
+askTen = askPrice + (0.1 * askPrice)
+askThirty = askPrice + (0.3 * askPrice)
+
+btcBalance = api.getbalance("BTC")['Available']
+
+print 'You have {} BTC available.'.format(btcBalance)
+print 'Ask -- ' + str(askPrice)
+print 'Ask + 10% (safeish buy point) -- ' + str(askTen)
+print 'Ask + 30% (safeish sell point) -- ' + str(askThirty)
+
+numCoins = (btcBalance - (btcBalance*0.025)) / askPrice
+
+print '\n[+] Buying {} {} coins at {} BTC each for a total of {} BTC'.format(numCoins,
+        pumpCoin, askTen, askTen*numCoins)
+
+if allow_orders:
+    print api.buylimit('BTC-' + pumpCoin, numCoins, askTen)
+else:
+    print "[!] allow_orders = False in script... change to make orders..."
+
+print '[+] Placing sell order at {} (+30%)...'.format(askThirty)
+
+if allow_orders:
+    print api.selllimit('BTC-' + pumpCoin, numCoins, askThrity)
+else:
+    print "[!] allow_orders = False in script... change to make orders..."
+
+
